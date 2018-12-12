244a4256c485a3a8e574b7107d98003e529e5211
xing614 <15210227194@163.com>
2018-12-12 10:15:46 +0800

    add 5+

---
+++ leetcodeWrite/src/medium/EncodeAndDecodeTinyURL_535.java
@@ -0,0 +1,32 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 535. TinyURL 的加密与解密
+ * 
+ * TinyURL是一种URL简化服务， 比如：当你输入一个URL https://leetcode.com/problems/design-tinyurl 时，它将返回一个简化的URL http://tinyurl.com/4e9iAk.
+
+要求：设计一个 TinyURL 的加密 encode 和解密 decode 的方法。你的加密和解密算法如何设计和运作是没有限制的，你只需要保证一个URL可以被加密成一个TinyURL，并且这个TinyURL可以用解密方法恢复成原本的URL。
+ * @author liang
+ *
+ */
+public class EncodeAndDecodeTinyURL_535 {
+    private static Map<String,String> map = new HashMap<>();
+    /**
+     * 最简单的 用hashCode
+     * @param longUrl
+     * @return
+     */
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String ec = "http://tinyurl.com/"+longUrl.hashCode();
+        map.put(ec,longUrl);
+        return ec;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl);
+    }
+}
